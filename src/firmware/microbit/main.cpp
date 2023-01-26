66388470ff67cf3f7585b29a1ec2d21272c82be0
BJamin99 <ben@bjamin.ca>
2023-01-26 18:28:00 -0500

    Support for using nonstandard pins for UART.

--- src/firmware/microbit/main.cpp
+++ src/firmware/microbit/main.cpp
@@ -9,6 +9,12 @@ MicroBit uBit;
 
 int main() {
     uBit.init();
+#ifdef YOTTA_CFG_TXPIN
+  #ifdef YOTTA_CFG_RXPIN
+    #pragma message("Bluing firmware will use custom serial pins")
+    uBit.serial.redirect(YOTTA_CFG_TXPIN, YOTTA_CFG_RXPIN);
+  #endif
+#endif
     timer_init(); // Init BLE timer.
     serial_init();
 
